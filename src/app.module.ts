10b6f4fc015ee9f7626444ac12c7d3a11de42b91
Girod <gwenael.girod@gmail.com>
2020-09-16 17:15:01 +0200

    Better .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,24 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {GamesModule} from "./games/games.module";
 import {MongooseModule} from "@nestjs/mongoose";
-import {DB_CONFIG} from "./config";
 import * as mongoose from "mongoose";
 import {APP_FILTER, APP_INTERCEPTOR} from "@nestjs/core";
 import {HttpErrorFilter} from "./shared/handler/http-error.filter";
 import {LoggingInterceptor} from "./shared/handler/logging.interceptor";
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import dotenv = require('dotenv');
+
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('runValidators', true);
+dotenv.config({path: "../.env.local"});
 
 @Module({
     imports: [
         ConfigModule.forRoot({isGlobal: true}),
         GamesModule,
-        MongooseModule.forRoot(DB_CONFIG.connectionString),
+        MongooseModule.forRoot(process.env.CONNECTION_STRING),
         UsersModule
     ],
     controllers: [
--- src/config.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Main config file
- */
-export const DB_CONFIG = {
-    connectionString: "mongodb+srv://gwenael:k2uZDLCjG8F9cLlz@cluster0.r0vxx.gcp.mongodb.net/gamerjuice?retryWrites=true&w=majority"
-};
