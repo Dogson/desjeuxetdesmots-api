55fc3eff9f6ce922e287d0ded677e534ec1c2d83
Girod <gwenael.girod@gmail.com>
2020-09-26 11:57:28 +0200

    Added dynamic port for heroku deployment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {addMediaTypeToEpisode} from "../scripts/addMediaTypeToEpisodes";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {cors: true});
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
