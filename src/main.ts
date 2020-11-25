d61749770dd8164130343f8b5e8d10b6923cf5b6
Gwen <gwenael.girod@gmail.com>
2020-11-25 21:36:28 +0100

    Increased payload size limit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import dotenv = require('dotenv');
 import {HttpErrorFilter} from "./shared/handler/http-error.filter";
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
     dotenv.config({path: "../.env"});
     const app = await NestFactory.create(AppModule, {cors: true});
     app.useGlobalFilters(new HttpErrorFilter());
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
     const server = await app.listen(process.env.PORT || 3000);
     server.setTimeout(3600000);
 }
